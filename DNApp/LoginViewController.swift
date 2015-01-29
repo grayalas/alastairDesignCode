44ff3305c4a007f35229cb7342ba3a1c177c1101
Meng To <shadownessguy@gmail.com>
2015-01-30 00:28:31 +0700

    Connecting Storyboard and Code

---
+++ DNApp/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  DNApp
+//
+//  Created by Meng To on 2015-01-29.
+//  Copyright (c) 2015 Meng To. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var dialogView: DesignableView!
+    
+    @IBAction func loginButtonDidTouch(sender: AnyObject) {
+        dialogView.animation = "shake"
+        dialogView.animate()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do something
+    }
+    
+}

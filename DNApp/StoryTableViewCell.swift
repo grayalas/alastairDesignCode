00f9113aa10eddf3520e475c04cb0af0a9f7bfe0
Meng To <shadownessguy@gmail.com>
2015-03-08 00:06:02 +0900

    TableViewCellClass

---
+++ DNApp/StoryTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  StoryTableViewCell.swift
+//  DNApp
+//
+//  Created by Meng To on 2015-03-07.
+//  Copyright (c) 2015 Meng To. All rights reserved.
+//
+
+import UIKit
+
+class StoryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var badgeImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var upvoteButton: SpringButton!
+    @IBOutlet weak var commentButton: SpringButton!
+    
+    @IBAction func upvoteButtonDidTouch(sender: AnyObject) {
+        upvoteButton.animation = "pop"
+        upvoteButton.force = 3
+        upvoteButton.animate()
+    }
+    
+    @IBAction func commentButtonDidTouch(sender: AnyObject) {
+        commentButton.animation = "pop"
+        commentButton.force = 3
+        commentButton.animate()
+    }
+    
+}
